2d7a20c0d496e7f1e45d030752cc41b3218916d7
AryanCS40 <aryanyadav26june@gmail.com>
2025-11-17 22:10:20 +0530

    Add initial files

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,6 @@
 {
     HOST_JWT_KEY = process.env.HOST_JWT_KEY,
     USER_JWT_KEY = process.env.USER_JWT_KEY,
-    SESSION_SECRET = process.env.SESSION_SECRET
-    
+    SESSION_SECRET = process.env.SESSION_SECRET    
 }
 
